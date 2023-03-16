e9e37d89f6983075e546425779766273c6adcb06
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2023-03-16 16:33:49 -0300

    Add Theme

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+    amarelo: '#FEBA1B',
+    verde: "#10C69D",
+    cinzaClaro: "#9b9b9b",
+    cinzaEscuro: "#131025",
+    cinzaSuperClaro: '#F4F4F4',
+    branco: '#FFFFFF'
+};
+
+export default theme;
